8073744e3de26722c7b7839ae81c8df655065ea6
Avarayr <mikamika83@gmail.com>
2021-11-04 23:38:29 -0700

    working version of classification

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,26 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import type { NextPage } from "next";
-import React, { useEffect, useRef, useState } from "react";
-import Webcam from "react-webcam";
+import Head from "next/head";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import Container from "../components/Container";
-import HintText from "../components/HintText";
 import HStack from "../components/HStack";
 import CogIcon from "../components/Icons/CogIcon";
 import PauseIcon from "../components/Icons/PauseIcon";
 import RightTriangle from "../components/Icons/RightTriangle";
 import SettingsMenu from "../components/SettingsMenu";
-import { useSettings } from "../contexts/Settings";
-import "@tensorflow/tfjs-core";
-import "@tensorflow/tfjs-converter";
-import "@tensorflow/tfjs-backend-webgl";
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import Head from "next/head";
+import WebcamScanner from "../components/WebcamScanner";
 
 const Home: NextPage = () => {
-  const [isScanning, setIsScanning] = useState(false);
-  const [showSettings, setShowSettings] = useState(false);
-
   const handleScanButtonClick = () => {
     setIsScanning(!isScanning);
   };
@@ -29,25 +20,8 @@ const Home: NextPage = () => {
     setShowSettings(!showSettings);
   };
 
-  const { camera, setCamera } = useSettings();
-
-  useEffect(() => {
-    const localCamera = localStorage.getItem("camera");
-    if (localCamera) {
-      setCamera(JSON.parse(localCamera));
-    }
-  }, [setCamera]);
-
-  const webcamRef = useRef(null);
-
-  const importCocoSsdModel = async () => {
-    const model = await cocoSsd.load();
-    console.log("The coco-ssd model has loaded.");
-  };
-
-  useEffect(() => {
-    importCocoSsdModel();
-  }, []);
+  const [isScanning, setIsScanning] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
   return (
     <>
@@ -56,22 +30,7 @@ const Home: NextPage = () => {
       </Head>
       <div>
         <Container>
-          {isScanning ? (
-            <Webcam
-              audio={false}
-              height={500}
-              width={500}
-              ref={webcamRef}
-              videoConstraints={{
-                deviceId: camera.deviceId,
-                facingMode: camera.facingMode,
-              }}
-            />
-          ) : (
-            <HintText>Tap to start scanning for 🐱</HintText>
-          )}
-
-          <div id="root"></div>
+          <WebcamScanner isScanning={isScanning} />
           <HStack>
             <Button onClick={handleScanButtonClick}>
               <AnimatePresence>
