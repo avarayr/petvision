89f686a5e76d543991f9e5d096857a607bccbd37
posdsu <80629531+posdsu@users.noreply.github.com>
2021-11-04 23:39:37 -0700

    added coco-ssd model (#14)
    * added coco-ssd model
    
    * added coco-ssd model
    
    * added coco model to deps
    
    * working version of classification
    
    Co-authored-by: patricia <patricia@patricias-MacBook-Air.local>
    Co-authored-by: Avarayr <mikamika83@gmail.com>

---
+++ frontend/components/WebcamScanner/index.tsx
@@ -0,0 +1,108 @@
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-converter";
+import * as tf from "@tensorflow/tfjs-core";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import { useSettings } from "../../contexts/Settings";
+import useDebounce from "../../hooks/useDebounce";
+import HintText from "../HintText";
+
+function WebcamScanner({ isScanning }: { isScanning: boolean }) {
+  const [detectedClass, setDetectedClass] = useState("");
+  const debouncedDetectedClass = useDebounce(detectedClass, 500);
+  const [cocoModel, setCocoModel] = useState<cocoSsd.ObjectDetection | null>(
+    null
+  );
+
+  const { camera, setCamera } = useSettings();
+
+  useEffect(() => {
+    const localCamera = localStorage.getItem("camera");
+    if (localCamera) {
+      setCamera(JSON.parse(localCamera));
+    }
+  }, [setCamera]);
+
+  const webcamRef = useRef(null);
+
+  const importCocoSsdModel = async () => {
+    await tf.setBackend("webgl");
+    setCocoModel(await cocoSsd.load());
+  };
+
+  const callPredict = (timeout = 500) => {
+    setTimeout(predictWebcam, timeout);
+  };
+
+  const predictWebcam = (event: any) => {
+    if (!webcamRef.current || !cocoModel) return;
+
+    cocoModel
+      .detect((webcamRef.current as any).video)
+      .then((predictions: any[]) => {
+        if (!predictions.length) {
+          setDetectedClass("");
+          callPredict();
+          return;
+        }
+        const { class: label, score } = predictions[0];
+        setDetectedClass(label);
+        isScanning && callPredict();
+      })
+      .catch(() => {
+        setDetectedClass("");
+        callPredict();
+      });
+  };
+
+  useEffect(() => {
+    importCocoSsdModel();
+  }, []);
+
+  return (
+    <>
+      {isScanning ? (
+        <>
+          <AnimatePresence>
+            {debouncedDetectedClass != "" && (
+              <motion.div
+                className="detected-class"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                style={{
+                  marginBottom: "1rem",
+                  position: "absolute",
+                  background: "white",
+                  padding: "0 50px",
+                  color: "black",
+                }}
+              >
+                <h1>{debouncedDetectedClass}</h1>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+          <Webcam
+            audio={false}
+            height={500}
+            width={500}
+            onLoadedData={predictWebcam}
+            ref={webcamRef}
+            videoConstraints={{
+              deviceId: camera.deviceId,
+              facingMode: camera.facingMode,
+            }}
+          />
+        </>
+      ) : (
+        <HintText>Tap to start scanning for 🐱</HintText>
+      )}
+    </>
+  );
+}
+
+export default WebcamScanner;
